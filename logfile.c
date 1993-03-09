c3d25e25e60fbeec3947ae71d7b69e6335643c10
gert <gert>
1993-03-09 22:23:44 +0000

    Initial revision

---
+++ logfile.c
@@ -0,0 +1,119 @@
+#ident "$Id: logfile.c,v 1.1 1993/03/09 22:23:44 gert Exp $ (c) Gert Doering"
+
+#include <stdio.h>
+#include <varargs.h>
+#include <sys/types.h>
+#include <time.h>
+#include <errno.h>
+#include <ctype.h>
+#include <fcntl.h>
+
+#include "mgetty.h"
+
+int log_level = L_MESG;        /* log all, changeable from main() */
+				/* or!!! getenv("LOGLEVEL");       */
+static FILE * log_fp;
+static boolean mail_logfile = FALSE;
+char log_path[ MAXPATH ];
+
+void logmail( void )
+{
+char ws[MAXPATH+100];
+    if ( mail_logfile )
+    {
+	sprintf( ws, "cat %s | mail -s \"BBS:fatal error!\" %s",
+		 log_path, ADMIN );
+	lprintf( L_MESG, "mailing logfile to %s...", ADMIN );
+	system( ws );
+    }
+}
+
+int lputc( int level, char ch )
+{
+    if ( log_fp != NULL && level <= log_level )
+    {
+	if ( isprint(ch) ) fputc( ch, log_fp );
+		      else fprintf( log_fp, "[%02x]", (unsigned char) ch );
+	fflush( log_fp );
+    }
+    return 0;
+}
+
+int lputs( int level, char * s )
+{
+int retcode = 0;
+    if ( log_fp != NULL && level <= log_level )
+    {
+	retcode = fputs( s, log_fp );
+	fflush( log_fp );
+    }
+    return retcode;
+}
+
+int lprintf( level, format, va_alist )
+int level;
+const char * format;
+va_dcl
+{
+char    ws[200];
+time_t  ti;
+struct tm *tm;
+va_list pvar;
+int     errnr;
+
+    va_start( pvar );
+
+    errnr = errno;
+
+    if ( log_fp == NULL )
+    {
+        if ( log_path[0] == 0 )
+	    sprintf( log_path, "%s/log_mgetty", LOG_PATH );
+	log_fp = fopen( log_path, "a" );
+	if ( log_fp == NULL )
+	{
+
+	    sprintf(ws, "cannot open logfile %s", log_path);
+	    perror(ws);
+	    exit(10);
+	}
+	if ( fileno( log_fp ) < 3 )
+	{
+	int fd;
+	    if ( ( fd = fcntl( fileno( log_fp ), F_DUPFD, 3 ) ) > 2 )
+	    {
+		fclose( log_fp );
+		log_fp = fdopen( fd, "a" );
+	    }
+	}
+	fprintf( log_fp, "\n--" );
+    }
+
+    vsprintf( ws, format, pvar );
+    va_end( pvar );
+
+    ti = time(NULL); tm = localtime(&ti);
+    if ( level <= log_level )
+    {
+	if ( level != L_ERROR && level != L_FATAL )
+	{
+	    fprintf(log_fp, "\n%d.%d./%02d:%02d:%02d  %s", tm->tm_mday, tm->tm_mon+1,
+			     tm->tm_hour, tm->tm_min, tm->tm_sec, ws );
+	    fflush(log_fp);
+	}
+	else
+	{
+	    fprintf(log_fp, "\n%d.%d./%02d:%02d:%02d  %s: %s", tm->tm_mday, tm->tm_mon+1,
+			     tm->tm_hour, tm->tm_min, tm->tm_sec, ws,
+			     ( errno <= sys_nerr ) ? sys_errlist[errno]:
+			     "<error not in list>" );
+	    fflush(log_fp);
+	    if ( level == L_FATAL )
+	    {
+		mail_logfile = TRUE;
+		atexit( logmail );
+	    }
+	}
+    }
+    return 0;
+}
