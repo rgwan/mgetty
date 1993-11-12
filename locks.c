2391093d62adeb07d8108adf691e0b93c30274a8
gert <gert>
1993-11-12 13:43:57 +0000

    add ";" after "#ident"

--- locks.c
+++ locks.c
@@ -1,4 +1,4 @@
-#ident "$Id: locks.c,v 1.18 1993/11/05 20:22:38 gert Exp $ Copyright (c) Gert Doering / Paul Sutcliffe Jr."
+#ident "$Id: locks.c,v 1.19 1993/11/12 13:43:57 gert Exp $ Copyright (c) Gert Doering / Paul Sutcliffe Jr.";
 
 /* large parts of the code in this module are taken from the
  * "getty kit 2.0" by Paul Sutcliffe, Jr., paul@devon.lns.pa.us,
