886b9f3421c69afcac56c3ca07129fe716bb2b07
gert <gert>
1994-03-08 15:46:42 +0000

    change make_utmp_wtmp to take ut_type / ut_name parameters

--- utmp.c
+++ utmp.c
@@ -1,4 +1,4 @@
-#ident "$Id: utmp.c,v 1.13 1994/01/21 11:36:48 gert Exp $ Copyright (c) Gert Doering"
+#ident "$Id: utmp.c,v 1.14 1994/03/08 15:46:42 gert Exp $ Copyright (c) Gert Doering"
 ;
 /* some parts of the code (writing of the utmp entry)
  * is based on the "getty kit 2.0" by Paul Sutcliffe, Jr.,
@@ -10,9 +10,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <utmp.h>
 
 #ifndef ENOENT
 #include <errno.h>
@@ -25,14 +25,15 @@ typedef short pid_t;
 #endif
 
 #include "mgetty.h"
+#include "mg_utmp.h"
 
 #if defined(sun) || defined(BSD)
 /* on SunOS (and other BSD-derived systems), the getty process does *
  * not have to care for the utmp entries, login and init do all the work
  * Anyway, we have to _read_ it to get the number of users logged in.
  */
-void make_utmp_wtmp _P2( (line, login_process),
-			 char * line, boolean login_process )
+void make_utmp_wtmp _P3( (line, ut_type, ut_user),
+			 char * line, short ut_type, char * ut_user )
 {
 }
 int get_current_users _P0( void )
@@ -42,16 +43,8 @@ int get_current_users _P0( void )
 }	/*! FIXME */
 #else
 
-/* define some prototypes - not all supported systems have these */
-#if !defined(SVR4) && !defined(linux) && !defined(__hpux)
-struct	utmp	*getutent _PROTO((void));
-struct	utmp	*pututline _PROTO((struct utmp * utmp));
-void		setutent _PROTO((void));
-void		endutent _PROTO((void));
-#endif
-
-void make_utmp_wtmp _P2( (line, login_process),
-			 char * line, boolean login_process )
+void make_utmp_wtmp _P3( (line, ut_type, ut_user),
+			 char * line, short ut_type, char * ut_user )
 {
 struct utmp *utmp;
 pid_t	pid;
@@ -69,16 +62,10 @@ FILE *	fp;
 
 	    utmp->ut_time = time( NULL );
 
-	    if ( login_process )
-	    {		/* show login process in utmp */
-		strcpy(utmp->ut_user, "LOGIN");
-		utmp->ut_type = LOGIN_PROCESS;
-	    }
-	    else
-	    {		/* still waiting for call */
-		strcpy(utmp->ut_user, "uugetty" );
-		utmp->ut_type = INIT_PROCESS;
-	    }
+	    utmp->ut_type = ut_type;	/* {INIT,LOGIN,USER}_PROCESS */
+	                                /* "LOGIN", "uugetty", "dialout" */
+	    strncpy( utmp->ut_user, ut_user, sizeof( utmp->ut_user ) );
+	    utmp->ut_user[ sizeof( utmp->ut_user ) -1 ] = 0;
 
 	    pututline(utmp);
 
