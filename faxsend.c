30575c3af32b339090e26fd1e02ecdd24e4198c4
gert <gert>
1994-04-24 12:49:42 +0000

    add "total bytes" log message add rigorous alarm()s during fax writes (modem lockups!)

--- faxsend.c
+++ faxsend.c
@@ -1,4 +1,4 @@
-#ident "$Id: faxsend.c,v 1.3 1994/03/01 23:04:59 gert Exp $ Copyright (c) 1994 Gert Doering"
+#ident "$Id: faxsend.c,v 1.4 1994/04/24 12:49:42 gert Exp $ Copyright (c) 1994 Gert Doering"
 ;
 /* faxsend.c
  *
@@ -30,9 +30,59 @@
 /* I don't know *why*, but the ZyXEL wants all bytes reversed */
 #define REVERSE 1
 
+static boolean fax_sendpg_timeout = FALSE;
+
 static RETSIGTYPE fax_send_timeout()
 {
-    lprintf( L_ERROR, "timeout!" );
+    signal( SIGALRM, fax_send_timeout );	/* reactivate handler */
+    
+    lprintf( L_WARN, "fax_send: timeout" );
+    fax_sendpg_timeout = TRUE;
+}
+
+/* DLE ETX: send at end of all fax data to terminate page */
+
+static	char	fax_end_of_page[] = { DLE, ETX };
+
+static void fax_send_panic_exit _P1( (fd), int fd )
+{
+    lprintf( L_FATAL, "PANIC: timeout sending fax page data, trying force modem reset\n" );
+
+    /* by all means, reset modem */
+
+    /* heavily use alarm(), to make sure nothing blocks */
+
+    /* flush output queue */
+    alarm( 5 ); tio_flush_queue( fd, TIO_Q_OUT );
+
+    /* restart possibly suspended output */
+    alarm( 5 ); tio_flow( fd, TRUE );
+
+    /* tell modem that the page is finished. Try twice */
+    alarm( 5 );	write( fd, fax_end_of_page, sizeof( fax_end_of_page ) );
+    
+    alarm( 5 );	write( fd, fax_end_of_page, sizeof( fax_end_of_page ) );
+
+    /* Hang up. Various methods */
+    alarm( 5 ); write( fd, "AT+FK\r\n", 7 );
+    alarm( 5 ); write( fd, "ATH0\r\n", 6 );
+
+    /* Now, try to reset it by lowering DTR */
+    alarm( 10 );
+    tio_toggle_dtr( fd, 500 );
+    delay(500);
+    tio_toggle_dtr( fd, 500 );
+    delay(500);
+
+    /* try again to hang up + reset it */
+    alarm( 5 ); write( fd, "ATH0Z\r\n", 7 );
+    delay(500);
+
+    /* if the modem is *still* off-hook, there's nothing we can do. */
+    /* Hope that mgetty will be able to reinitialize it */
+
+    alarm( 0 );
+    exit(11);
 }
 
 /* fax_send_page - send one complete fax-G3-file to the modem
@@ -49,7 +99,7 @@ char ch;
 char buf[256];
 char wbuf[ sizeof(buf) * 2 ];
 
-static	char	fax_end_of_page[] = { DLE, ETX };
+int w_total = 0;		/* total bytes written */
 
     lprintf( L_NOISE, "fax_send_page(\"%s\") started...", g3_file );
 
@@ -114,10 +164,20 @@ static	char	fax_end_of_page[] = { DLE, ETX };
     else
     {
 	int r, i, w;
+	int w_refresh = 0;
 	boolean first = TRUE;
+
+	alarm( 30 );		/* timeout if we get stuck in flow control */
 
 	while ( ( r = read( g3fd, buf, 64 ) ) > 0 )
 	{
+	    /* refresh alarm counter every 1000 bytes */
+	    if ( w_refresh > 1000 )
+	    {
+		w_refresh -= 1000;
+		alarm( 30 );
+	    }
+	    
 	    i = 0;
 	    /* skip over GhostScript / digifaxhigh header */
 
@@ -158,6 +218,8 @@ static	char	fax_end_of_page[] = { DLE, ETX };
 		}
 	    }
 
+	    /* escape DLE characters. If necessary (rockjunk!), swap bits */
+	    
 	    for ( w = 0; i < r; i++ )
 	    {
 #if REVERSE
@@ -175,6 +237,16 @@ static	char	fax_end_of_page[] = { DLE, ETX };
 		lprintf( L_ERROR, "could not write all %d bytes", w );
 	    }
 
+	    /* check for timeout */
+
+	    if ( fax_sendpg_timeout )
+	    {
+		fax_send_panic_exit( fd );
+	    }
+
+	    w_total += w;
+	    w_refresh += w;
+	    
 	    /* drain output */
 	    /* well, since the handshake stuff seems to work now,
 	     * this shouldn't be necessary anymore (but if you have
@@ -218,7 +290,11 @@ static	char	fax_end_of_page[] = { DLE, ETX };
     lprintf( L_NOISE, "sending DLE ETX..." );
     write( fd, fax_end_of_page, sizeof( fax_end_of_page ));
 
+    alarm(0);
+
     if ( fax_wait_for( "OK", fd ) == ERROR ) return ERROR;
+
+    lprintf( L_MESG, "page complete, %d bytes sent", w_total );
 
     return NOERROR;
 }
