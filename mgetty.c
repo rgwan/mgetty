8f4ca36b072585b06e8b686c4d44f34aa6c5b29a
gert <gert>
1995-08-09 16:53:51 +0000

    NeXT fixes by Christian Starkjohann

--- mgetty.c
+++ mgetty.c
@@ -1,4 +1,4 @@
-#ident "$Id: mgetty.c,v 2.19 1995/08/06 18:27:37 gert Exp $ Copyright (c) Gert Doering"
+#ident "$Id: mgetty.c,v 2.20 1995/08/09 16:53:51 gert Exp $ Copyright (c) Gert Doering"
 
 /* mgetty.c
  *
@@ -892,6 +892,11 @@ Ring_got_action:
 	    if ( t != NULL )
 	        { sprintf( t, "TERM=%s", c_string(termtype) ); putenv(t); }
 	}
+
+#ifdef NeXT
+	/* work around NeXT's weird problems with POSIX termios vs. sgtty */
+	NeXT_repair_line(STDIN);
+#endif
 	
 	/* hand off to login dispatcher (which will call /bin/login) */
 	login_dispatch( buf );
