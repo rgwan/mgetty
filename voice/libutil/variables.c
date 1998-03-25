dd3b6804682bd22aa632e79cd6d49314207ebbc9
marc <marc>
1998-03-25 23:14:08 +0000

    * New file

---
+++ voice/libutil/variables.c
@@ -0,0 +1,13 @@
+/*
+ * variables.c
+ *
+ * Just defines necessary global variables.
+ *
+ * $Id: variables.c,v 1.1 1998/03/25 23:14:08 marc Exp $
+ *
+ */
+
+#include "../include/voice.h"
+#include "../include/version.h"
+
+char POS[80];
