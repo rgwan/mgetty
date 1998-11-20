7c56c9441cd04a3d0930355bf443e78f71a5dee6
gert <gert>
1998-11-20 10:06:44 +0000

    move check for $fax_spool_out to wwwsub.pl

--- frontends/www/wwwsub.pl
+++ frontends/www/wwwsub.pl
@@ -1,7 +1,7 @@
 #
 # mgetty WWW gui common "things"
 #
-# RCS: $Id: wwwsub.pl,v 1.1 1998/10/22 12:54:49 gert Exp $
+# RCS: $Id: wwwsub.pl,v 1.2 1998/11/20 10:06:47 gert Exp $
 #
 # $Log
 
@@ -14,6 +14,19 @@ sub errormessage
 </body></html>
 EOF
     exit 1;
+}
+
+sub check_outgoing
+{
+    # check, if fax-outgoing is defined
+    if ($fax_outgoing eq "")
+    {
+	errormessage("\$fax_outgoing : not configured - please look at the configuration-Files and configure the Directory where you store your outgoing faxes (\$fax_outgoing)");
+    }
+    if (! -d $fax_outgoing)
+    { errormessage("\$fax_outgoing : no such directory $fax_outgoing");}
+    if (! -r $fax_outgoing)
+    { errormessage("\$fax_outgoing : no read-permission for $fax_outgoing");}
 }
 
 1;
