2e8c91558486df2470c2b2f84df4fecfa4200b74
gert <gert>
2005-09-10 16:23:15 +0000

    Actually import lprintf() and friends (do away with null functions) set log level (-x<n>) and log path (no option yet, always /tmp/atsms.log) query modem for SMS PIN status    send PIN if specified (-p <nnn>) and necessary move around some status messages copy & paste mdm_command() from modem.c, add configurable timeout setting    (sending PIN to GSM modem can take 20-30 seconds or longer)

--- tap/atsms.c
+++ tap/atsms.c
@@ -1,4 +1,4 @@
-#ident "$Id: atsms.c,v 1.1 2005/09/01 09:41:19 gert Exp $ Copyright (c) Gert Doering"
+#ident "$Id: atsms.c,v 1.2 2005/09/10 16:23:15 gert Exp $ Copyright (c) Gert Doering"
 
 /* atsms.c
  *
@@ -20,29 +20,21 @@
 #include "policy.h"
 #include "tio.h"
 
-/* we don't want logging here */
-#ifdef USE_VARARGS
-int lprintf() { return 0; }
-#else
-int lprintf(int level, const char *format, ...) { return 0; }
-#endif
-int lputs( int level, char * string ) { return 0; }
-int lputc( int level, char ch ) { return 0; }
-
 /* interrupt handler for SIGALARM */
 boolean got_interrupt;
 RETSIGTYPE oops(SIG_HDLR_ARGS)
 		{ got_interrupt=TRUE; }
 
-int send_sms( char * device, int speed, char * sms_to, char * sms_text )
+int send_sms( char * device, int speed, char * sim_pin, 
+		char * sms_to, char * sms_text )
 {
 int fd;
 TIO tio, save_tio;
 
-char buf[200];
+char buf[200], *p;
 int l;
 
-    printf( "send SMS message to \"%s\"...\nMSG: \"%s\"\n", sms_to, sms_text );
+    printf( "send SMS message to \"%s\"...\n", sms_to );
 
     /* lock device */
     if ( makelock( device ) == FAIL )
@@ -92,6 +84,41 @@ int l;
 	close(fd); rmlocks(); return -1;
     }
 
+    /* PIN needed? */
+    p = mdm_get_idstring( "AT+CPIN?", 1, fd );
+    if ( p == NULL || strcmp( p, "<ERROR>" ) == 0 )
+    {
+	fprintf( stderr, "can't query modem on '%s' for PIN status: '%s'\n",
+		 device, p );
+	close(fd); rmlocks(); return -1;
+    }
+
+    if ( strcmp( p, "+CPIN: SIM PIN" ) == 0 )
+    {
+	if ( sim_pin == NULL || sim_pin[0] == '\0' )
+	{
+	    fprintf( stderr, "modem on '%s' wants PIN, but none specified\n", 
+			device );
+	    close(fd); rmlocks(); return -1;
+	}
+	printf( "GSM modem wants PIN, please wait (up to 2 minutes)...\n" );
+	char sbuf[100];
+	sprintf( sbuf, "AT+CPIN=%.20s", sim_pin );
+	if ( mdm_command_timeout( sbuf, fd, 120 ) == ERROR )
+	{
+	    fprintf( stderr, "PIN '%s' not accepted on modem on '%s'\n",
+			sim_pin, device );
+	    close(fd); rmlocks(); return -1;
+	}
+	printf( "OK!\n" );
+    }
+    else
+      if ( strcmp( p, "+CPIN: READY" ) != 0 )
+    {
+	fprintf( stderr, "unexpected response to PIN query: '%s'\n", p );
+	close(fd); rmlocks(); return -1;
+    }
+
     /* enter SMS text mode */
     if ( mdm_command( "AT+CMGF=1", fd ) == ERROR )
     {
@@ -110,6 +137,7 @@ int l;
     /* TODO: wait for '>' prompt */
     delay(100);
 
+    printf( "MSG: \"%s\"\n", sms_text );
     if ( mdm_send( sms_text, fd ) == ERROR ||
          write( fd, "\032", 1 ) != 1 )		/* ctrl-Z as term.chr. */
     {
@@ -166,13 +194,19 @@ int opt;
 char * device = "/dev/ttyh1";		/* TODO */
 
 int speed = 38400;				/* port speed */
+char * sim_pin = NULL;				/* pin number */
 
-    while ((opt = getopt(argc, argv, "l:s:x:")) != EOF)
+    log_init_paths( argv[0], "/tmp/atsms.log", NULL );
+    log_set_llevel(9);
+
+    while ((opt = getopt(argc, argv, "l:s:x:p:")) != EOF)
     {
 	switch( opt )
 	{
 	    case 'l': device = optarg; break;
 	    case 's': speed = atoi(optarg); break;
+	    case 'p': sim_pin = optarg; break;
+	    case 'x': log_set_llevel( atoi(optarg) ); break;
 	    default:
 		exit_usage( argv[0], NULL );
 	}
@@ -194,8 +228,45 @@ int speed = 38400;				/* port speed */
     siginterrupt( SIGHUP,  TRUE );
 #endif
 
-    send_sms( device, speed, argv[optind], argv[optind+1] );
+    send_sms( device, speed, sim_pin, argv[optind], argv[optind+1] );
 
     return 0;
 }
 
+static boolean fwf_timeout = FALSE;
+static RETSIGTYPE fwf_sig_alarm(SIG_HDLR_ARGS)      	/* SIGALRM handler */
+{
+    signal( SIGALRM, fwf_sig_alarm );
+    lprintf( L_WARN, "Warning: got alarm signal!" );
+    fwf_timeout = TRUE;
+}
+int mdm_command_timeout (char * send, int fd, int timeout )
+{
+    char * l;
+    
+    if ( mdm_send( send, fd ) == ERROR ) return ERROR;
+
+    /* wait for OK or ERROR, *without* side effects (as fax_wait_for
+     * would have)
+     */
+    signal( SIGALRM, fwf_sig_alarm ); alarm(timeout); fwf_timeout = FALSE;
+
+    do
+    {
+	l = mdm_get_line( fd );
+	if ( l == NULL ) break;
+	lprintf( L_NOISE, "mdm_command: string '%s'", l );
+    }
+    while ( strcmp( l, "OK" ) != 0 && strcmp( l, "ERROR" ) != 0 );
+
+    alarm(0); signal( SIGALRM, SIG_DFL );
+    
+    if ( l == NULL || strcmp( l, "ERROR" ) == 0 )
+    {
+	lputs( L_MESG, " -> ERROR" );
+	return ERROR;
+    }
+    lputs( L_MESG, " -> OK" );
+	
+    return NOERROR;
+}
