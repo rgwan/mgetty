55f419b93c4d4fa682c4568bbd224f27e6c415a1
gert <gert>
2010-06-05 09:57:50 +0000

    new version released

--- version.h
+++ version.h
@@ -1,3 +1,3 @@
-#define VERSION_LONG "interim release 1.1.36-Mar19";
-#define VERSION_SHORT "1.1.36"
+#define VERSION_LONG "interim release 1.1.37-Jun05";
+#define VERSION_SHORT "1.1.37"
 extern char * mgetty_version;
