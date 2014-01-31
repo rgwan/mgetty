3c90987cfb90988f27b6e44420992a9164b857e3
Gert Doering <gert@greenie.muc.de>
2014-01-31 10:06:09 +0000

    move clean_line() from do_chat.c to modem.c (so sendfax can use it)

--- do_chat.c
+++ do_chat.c
@@ -1,4 +1,4 @@
-#ident "$Id: do_chat.c,v 4.2 2003/10/05 11:58:57 gert Exp $ Copyright (c) Gert Doering"
+#ident "$Id: do_chat.c,v 4.3 2014/01/31 10:06:09 gert Exp $ Copyright (c) Gert Doering"
 
 /* do_chat.c
  *
@@ -233,64 +233,4 @@ static	char	*lptr = lbuf;
     tio_set( fd, &save_tio );
 
     return retcode;
-}
-
-/* clean_line()
- *
- * wait for the line "fd" to be silent for "waittime" tenths of a second
- * if more than 500 bytes are read, stop logging them. We don't want to
- * have the log files fill up all of the hard disk.
- */
-
-int clean_line _P2 ((fd, waittime), int fd, int waittime )
-{
-    char buffer[2];
-    int	 bytes = 0;				/* bytes read */
-
-#if defined(MEIBE) || defined(NEXTSGTTY) || defined(BROKEN_VTIME)
-    /* on some systems, the VMIN/VTIME mechanism is obviously totally
-     * broken. So, use a select()/flush queue loop instead.
-     */
-    lprintf( L_NOISE, "waiting for line to clear (select/%d ms), read: ", waittime * 100 );
-
-    while( wait_for_input( fd, waittime * 100 ) &&
-	   read( fd, buffer, 1 ) > 0 &&
-	   bytes < 10000 )
-    {
-	if ( ++bytes < 500 ) lputc( L_NOISE, buffer[0] );
-    }
-#else
-TIO	tio, save_tio;
-
-    lprintf( L_NOISE, "waiting for line to clear (VTIME=%d), read: ", waittime);
-
-    /* set terminal timeout to "waittime" tenth of a second */
-    tio_get( fd, &tio );
-    save_tio = tio;				/*!! FIXME - sgtty?! */
-    tio.c_lflag &= ~ICANON;
-    tio.c_cc[VMIN] = 0;
-    tio.c_cc[VTIME] = waittime;
-    tio_set( fd, &tio );
-
-    /* read everything that comes from modem until a timeout occurs */
-    while ( read( fd, buffer, 1 ) > 0 &&
-	    bytes < 10000 )
-    {
-        if ( ++bytes < 500 ) lputc( L_NOISE, buffer[0] );
-    }
-
-    /* reset terminal settings */
-    tio_set( fd, &save_tio );
-    
-#endif
-
-    if ( bytes > 500 )
-        lprintf( L_WARN, "clean_line: only 500 of %d bytes logged", bytes );
-    if ( bytes >= 10000 )
-    {
-	extern char * Device;
-        lprintf( L_FATAL, "clean_line: got too much junk (dev=%s).", Device );
-    }
-    
-    return 0;
 }
--- modem.c
+++ modem.c
@@ -1,4 +1,4 @@
-#ident "$Id: modem.c,v 4.6 2010/09/17 15:37:44 gert Exp $ Copyright (c) Gert Doering"
+#ident "$Id: modem.c,v 4.7 2014/01/31 10:06:09 gert Exp $ Copyright (c) Gert Doering"
 
 /* modem.c
  *
@@ -199,4 +199,66 @@ char * mdm_get_idstring _P3( (send, n, fd), char * send, int n, int fd )
     if ( l == NULL ) return "<ERROR>";			/* error */
 
     return rbuf;
+}
+
+
+/* clean_line()
+ *
+ * wait for the line "fd" to be silent for "waittime" tenths of a second
+ * if more than 500 bytes are read, stop logging them. We don't want to
+ * have the log files fill up all of the hard disk.
+ */
+
+int clean_line _P2 ((fd, waittime), int fd, int waittime )
+{
+    char buffer[2];
+    int	 bytes = 0;				/* bytes read */
+
+#if defined(MEIBE) || defined(NEXTSGTTY) || defined(BROKEN_VTIME) ||\
+	defined(FAX_SEND_SOCKETS)
+    /* on some systems, the VMIN/VTIME mechanism is obviously totally
+     * broken. So, use a select()/flush queue loop instead.
+     */
+    lprintf( L_NOISE, "waiting for line to clear (select/%d ms), read: ", waittime * 100 );
+
+    while( wait_for_input( fd, waittime * 100 ) &&
+	   read( fd, buffer, 1 ) > 0 &&
+	   bytes < 10000 )
+    {
+	if ( ++bytes < 500 ) lputc( L_NOISE, buffer[0] );
+    }
+#else
+TIO	tio, save_tio;
+
+    lprintf( L_NOISE, "waiting for line to clear (VTIME=%d), read: ", waittime);
+
+    /* set terminal timeout to "waittime" tenth of a second */
+    tio_get( fd, &tio );
+    save_tio = tio;				/*!! FIXME - sgtty?! */
+    tio.c_lflag &= ~ICANON;
+    tio.c_cc[VMIN] = 0;
+    tio.c_cc[VTIME] = waittime;
+    tio_set( fd, &tio );
+
+    /* read everything that comes from modem until a timeout occurs */
+    while ( read( fd, buffer, 1 ) > 0 &&
+	    bytes < 10000 )
+    {
+        if ( ++bytes < 500 ) lputc( L_NOISE, buffer[0] );
+    }
+
+    /* reset terminal settings */
+    tio_set( fd, &save_tio );
+    
+#endif
+
+    if ( bytes > 500 )
+        lprintf( L_WARN, "clean_line: only 500 of %d bytes logged", bytes );
+    if ( bytes >= 10000 )
+    {
+	extern char * Device;
+        lprintf( L_FATAL, "clean_line: got too much junk (dev=%s).", Device );
+    }
+    
+    return 0;
 }
